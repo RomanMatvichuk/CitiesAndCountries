ac919984627f2fa1fe062b2a311f0a116ed2b8c4
Roman Matvichuk <54710397+RomanMatvichuk@users.noreply.github.com>
2019-09-25 15:36:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-var cities = [{}];
-var countries = {};
-
 fetch('stad.json')
     .then(response => response.json())
     .catch(error => document.write(error))
     .then(function(myJson){
         myJson.sort((a, b) => a.countryid - b.countryid  || b.population - a.population);
-        cities=myJson;
+        console.log(myJson);
     })
 
 fetch('land.json')
@@ -14,19 +11,19 @@ fetch('land.json')
     .catch(error => document.write(error))
     .then(function(myJson){
         myJson.sort((a, b) => (a.countryname < b.countryname) ? -1 : 1);
-        countries=myJson;
+        console.log(myJson);
     })
 
-console.log(cities);
-console.log(countries);
+//console.log(cities);
+//console.log(countries);
 
-var htmlLines = "";
+/*var htmlLines = "";
 
 for (let i = 0; i < countries.length; i++) {
     htmlLines += "<li><h3>" + countries[i].countryname + "</h3></li>";
-        /* for (let i1 = 0; countries[i].id = cities[i1].countryid; i1++) {
+        for (let i1 = 0; countries[i].id = cities[i1].countryid; i1++) {
             htmlLines += "<li>" + cities[i1].stadname + " med " + cities[i1].population + " invånare</li>";            
-        } */
+        } 
 }
 
-document.getElementById("list").innerHTML = htmlLines;
+document.getElementById("list").innerHTML = htmlLines;*/
